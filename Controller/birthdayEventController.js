eab2e5249f61ec0856be633c492dd2e93c264b65
Kaiisea <abacatemarinheiro@gmail.com>
2022-07-14 14:50:38 +0200

    FIXED: birthday backend bug

--- Controller/birthdayEventController.js
+++ Controller/birthdayEventController.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Model = require("../Model/specialEventModel");
+const Model = require("../Model/birthdayEventModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
